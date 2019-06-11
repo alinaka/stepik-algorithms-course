2bca5d1a68b15fc98ff9abeae2f4c94015b4ed7f
Alina Gotovtseva <alinakago@gmail.com>
2019-06-12 01:15:51 +0300

    adds fractional knapsack practice decision

---
+++ greedy/fractional_knapsack/__init__.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def fractional_knapsack(capacity, values_and_weights):
+    order = [(v/w, w ) for v, w in values_and_weights]
+    order.sort(reverse=True)
+
+    acc = 0
+    for v_per_w, w in order:
+        if w < capacity:
+            acc += v_per_w * w
+            capacity -= w
+        else:
+            acc += v_per_w * capacity
+            break
+    return acc
+
+
+def main():
+    reader = (tuple(map(int, line.split())) for line in sys.stdin)
+    n, capacity = next(reader)
+    values_and_weights = list(reader)
+    assert len(values_and_weights) == n
+    opt_value = fractional_knapsack(capacity, values_and_weights)
+    print(f"{opt_value:.3f}")
+
+
+if __name__ == "__main__":
+    main()
--- intro/timing.py
+++ intro/timing.py
@@ -2,11 +2,11 @@ import time
 from matplotlib import pyplot as plt
 
 
-def timed(f, args, n_iter=100):
+def timed(f, *args, n_iter=100):
     acc = float("inf")
     for i in range(n_iter):
         t0 = time.perf_counter()
-        f(args)
+        f(*args)
         t1 = time.perf_counter()
         acc = min(acc, t1 - t0)
     return acc
